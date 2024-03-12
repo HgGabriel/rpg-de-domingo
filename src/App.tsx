0e098bc599b0ca0f87dbf829a0dbb07c32fdb0fe
HgGabriel <hiagogabrieloliveirapinto@gmail.com>
2024-03-12 20:06:42 -0300

    Conceitulizada UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './index.css'
-// import Landing from './components/Landing/Landing.tsx'
-import Home from './components/Home/Home.tsx'
+import Landing from './components/Landing/Landing.tsx'
+import Home from './components/Home/Home.tsx';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Home />
+    <>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Landing />}/>
+    </Routes>
+    </>
   );
 }
 
