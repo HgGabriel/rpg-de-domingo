3eaf3fe22ce4e7378cc666450f28f3e4c53c7615
Infran <64187114+Infran@users.noreply.github.com>
2024-03-13 00:45:24 +0000

    docker try

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,10 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <div>
-        <a href="https://dnd.wizards.com/" target="_blank">
-          <img src={dndlogo} className="logo" alt="D&D logo" />
-        </a>
-      </div>
-      <h1>Dungeons & Dragons</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> 
-      </div>
-      <p className="read-the-docs">
-        Click on the D&D logo for help
-      </p>
-=======
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/' element={<Landing />}/>
     </Routes>
->>>>>>> 95b1c60c49ae6af12f73380af839287252474662
     </>
   );
 }
